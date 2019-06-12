3b91ea57371fc5fe08e43b14bcf4bb0d8e4e5e15
RafaelGSS <rafael.nunu@hotmail.com>
2019-06-11 22:16:07 -0300

    update: adjusted seed

--- ms-order/database/seeds/orders.js
+++ ms-order/database/seeds/orders.js
@@ -1,13 +1,13 @@
 
-exports.seed = function(knex, Promise) {
+exports.seed = function (knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('orders').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('orders').insert([
+        { id: 1, id_user: 1, description: '', approved: true },
+        { id: 2, id_user: 1, description: '', approved: true },
+        { id: 3, id_user: 1, description: '', approved: false },
       ]);
     });
 };
