fabaed2d57356d53061d2bac9ec8ca0fb3c565f4
RafaelGSS <rafael.nunu@hotmail.com>
2019-07-01 22:38:46 -0300

    feat: Add fastify-amqp

--- ms-order/server.js
+++ ms-order/server.js
@@ -7,6 +7,26 @@ const AutoLoad = require('fastify-autoload')
 
 const path = require('path')
 
+fastify.register(require('fastify-amqp'), {
+  host: 'rabbitmq'
+})
+
+// Cron for create order
+// TODO: Continue it
+const queue = 'users_created'
+fastify.amqpChannel.assertQueue(queue)
+fastify.amqpChannel.consume(queue, function (msg) {
+  if (msg !== null) {
+    console.log('%s Received: %s', new Date(), msg.content.toString())
+    fastify.inject({
+      method: 'POST',
+      url: '/',
+      payload: ''
+    })
+    ch.ack(msg)
+  }
+})
+
 fastify.register(AutoLoad, {
   dir: path.join(__dirname, 'services')
 })
